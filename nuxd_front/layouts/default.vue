d78cd312599f5ed76e392390fc86d16e9a95753f
izzhamburhan <maizzham@gmail.com>
2024-04-25 11:59:18 +0800

    front : add user authentication (store/user.js)

--- nuxd_front/layouts/default.vue
+++ nuxd_front/layouts/default.vue
@@ -1,4 +1,11 @@
-<!-- default.vue -->
+
+<script setup>
+import { useUserStore} from '@/stores/user'
+
+const userStore = useUserStore()
+
+</script>
+
 <template>
     <div>
       <!-- Navigation -->
@@ -21,8 +28,15 @@
         <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
           <p class="text-gray-300 text-center md:text-left">Copyright &copy; 2024 - NuxD</p>
           <div class="flex mt-4 md:mt-0 items-center space-x-4">
-            <NuxtLink to="/login" class="py-2 px-4 bg-green-600 hover:bg-green-500 text-white rounded-lg transition duration-300">Log In</NuxtLink>
-            <NuxtLink to="/signup" class="py-2 px-4 bg-green-400 hover:bg-green-300 text-white rounded-lg transition duration-300">Sign Up</NuxtLink>
+            <template v-if="userStore.user.isAuthenticated">
+              <NuxtLink to="/myjobs" class="py-2 px-4 bg-green-600 hover:bg-green-500 text-white rounded-lg transition duration-300">My Jobs</NuxtLink>
+                <NuxtLink to="/createjob" class="py-2 px-4 bg-green-400 hover:bg-green-300 text-white rounded-lg transition duration-300">Create Jobs</NuxtLink>
+                <NuxtLink to="/logout" class="py-2 px-4 bg-red-400 hover:bg-red-300 text-white rounded-lg transition duration-300">Log Out</NuxtLink>
+              </template>
+              <template v-else>
+                <NuxtLink to="/login" class="py-2 px-4 bg-green-600 hover:bg-green-500 text-white rounded-lg transition duration-300">Log In</NuxtLink>
+                <NuxtLink to="/signup" class="py-2 px-4 bg-green-400 hover:bg-green-300 text-white rounded-lg transition duration-300">Sign Up</NuxtLink>
+            </template>
           </div>
         </div>
       </footer>
