99ea86e412bcc63cf3ffdcae3f631d00f3a5cdf4
izzhamburhan <maizzham@gmail.com>
2024-04-25 10:03:50 +0800

    add frontend folder

---
+++ nuxd_front/layouts/default.vue
@@ -0,0 +1,30 @@
+<!-- default.vue -->
+<template>
+    <div>
+      <!-- Navigation -->
+      <nav class="p-6 flex items-center justify-between bg-gradient-to-r from-black to-green-600">
+        <NuxtLink to="/" class="text-2xl text-white font-bold">NuxD</NuxtLink>
+  
+        <div class="flex items-center space-x-4">
+          <NuxtLink to="/" class="text-xl text-white hover:text-teal-300 font-mono">Home</NuxtLink>
+          <NuxtLink to="/browse" class="text-xl text-white hover:text-teal-300 font-mono">Browse</NuxtLink>
+        </div>
+      </nav>
+  
+      <!-- Main content -->
+      <div>
+        <slot />
+      </div>
+  
+      <!-- Footer -->
+      <footer class="p-6 bg-gradient-to-r from-black to-green-600">
+        <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+          <p class="text-gray-300 text-center md:text-left">Copyright &copy; 2024 - NuxD</p>
+          <div class="flex mt-4 md:mt-0 items-center space-x-4">
+            <NuxtLink to="/login" class="py-2 px-4 bg-green-600 hover:bg-green-500 text-white rounded-lg transition duration-300">Log In</NuxtLink>
+            <NuxtLink to="/signup" class="py-2 px-4 bg-green-400 hover:bg-green-300 text-white rounded-lg transition duration-300">Sign Up</NuxtLink>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </template>
---
+++ nuxd_front/pages/index.vue
@@ -0,0 +1,59 @@
+<!-- index.vue -->
+<template>
+    <div>
+         <div class="py-20 px-6 bg-gradient-to-r from-black to-green-600 text-center font-mono font-bold">
+             <h1 class="mb-12 text-5xl text-white">Find a job anywhere</h1>
+ 
+             <NuxtLink to="/browse" class="inline-block py-4 px-6 bg-green-800 hover:bg-green-700 text-white rounded-xl">Browse</NuxtLink>
+         </div>
+ 
+         <div class="py-10 px-6 bg-gradient-to-r from-black to-green-600">
+             <h2 class="mb-8 text-2xl text-center text-white font-mono">Newest Job</h2>
+ 
+             <div class="space-y-4">
+                 <div class="p-6 flex items-center justify-between bg-gray-100 rounded-xl">
+                     <div>
+                         <h3 class="mb-2 text-xl font-semibold">The Job Position</h3>
+                         <p class="text-gray=600">The Company Name</p>
+                     </div>
+ 
+                     <div>
+                         <p class="mb-2">WorldWide</p>
+                         <p>RM30-50k</p>
+                     </div>
+ 
+                     <div>
+                         <p>Posted Dec. 1 2022</p>
+                     </div>
+ 
+                     <div>
+                         <NuxtLink to="/browse/1" class="py-4 px-6 bg-green-700 hover:bg-green-600 text-white rounded-xl">Details</NuxtLink>
+                     </div>
+                 </div>
+ 
+                 <div class="space-y-4">
+                     <div class="p-6 flex items-center justify-between bg-gray-100 rounded-xl">
+                         <div>
+                             <h3 class="mb-2 text-xl font-semibold">The Job Position</h3>
+                             <p class="text-gray=600">The Company Name</p>
+                         </div>
+ 
+                         <div>
+                             <p class="mb-2">WorldWide</p>
+                             <p>RM30-50k</p>
+                         </div>
+ 
+                         <div>
+                             <p>Posted Dec. 1 2022</p>
+                         </div>
+ 
+                         <div>
+                             <NuxtLink to="/browse/1" class="py-4 px-6 bg-green-700 hover:bg-green-600 text-white rounded-xl">Details</NuxtLink>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </template>
+ 
